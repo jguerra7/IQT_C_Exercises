25433966530566390f6eec7188d02f62cc806d78
station5 <station5@Station5.int.net>
2019-04-22 14:32:45 -0500

    Completed Operational Expression exercises

--- operational_expression.c
+++ operational_expression.c
@@ -3,17 +3,46 @@ double sqrt(double x);
 
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
+
+// Helper Functions
+double get_leg_value();
 
 int main(void) 
 {
     /* insert initialize variables here */
-    int a,b,c = 0;
+    
+    double a, b, c= 0;
     /* insert input values and assign to variables here */
-    if (/*insert logical combo of relational expressions here */)
-    {
-        /* insert arithmetic hypotenuse calculation here */
-        /* insert -Print the hypotenuse here */
+    printf("Please input the first leg (A)\n");
+    a = get_leg_value();
+
+    printf("Please input the second leg (B)\n");
+    b = get_leg_value();
+
+    printf("Leg A: %.2lf\n", a);
+    printf("Leg B: %.2lf\n", b);
+
+    if(isnan(a) || isnan(b)) {
+        c = NAN;
     }
+    else {
+        c = sqrt((a * a) + (b *b));
+    }
+
+    printf("Hypotenuse: %.2lf\n", c);
     return 0;
+}
+
+double get_leg_value() {
+    char buffer[128] = {0};
+    if(fgets(buffer, sizeof(buffer), stdin) != NULL) {
+        double leg_value = strtod(buffer, NULL);
+        if(leg_value > 0.0) {
+            return leg_value;
+        }
+    }
+
+    return NAN;
 }
